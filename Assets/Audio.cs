3028651669cd9ac143a917cea3babfdbeb275822
MatiasARSdev <105838341+MatiasARSdev@users.noreply.github.com>
2023-01-09 01:49:41 -0300

    09/01

---
+++ Assets/Audio.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class soundTrigger2 : MonoBehaviour
+{
+    public AudioSource sonido;
+    
+    // Start is called before the first frame update
+    private void OnTriggerEnter(Collider other)
+    {
+         sonido.Play();
+        
+        
+    }
+}
